e0965c36fc37a33ee4be5513a0c843fbba2c40c4
TechLion Dev (via GIT) <git@techlion.dev>
2024-05-27 13:28:43 -0400

    feat: Add expand functionality to DatePill component
    The expand functionality is added to the DatePill component to retrieve additional data such as copticDate, facts, icons, and stories from the occasion collection. This allows for a more comprehensive display of information for a selected date.

--- src/components/date-pill.tsx
+++ src/components/date-pill.tsx
@@ -14,11 +14,25 @@ function DatePill({ data, setData }: any) {
     activeModifiers: any,
     e: any
   ) {
+    console.log(`Looking for: ${day.toISOString().split("T")[0]}`);
     const record = await pb.collection("occasion").getList(1, 10, {
       sort: "-created",
-      filter: 'date ~ "' + day.toISOString().split("T")[0] + '"'
+      filter: 'date ~ "' + day.toISOString().split("T")[0] + '"',
+      expand: "copticDate,facts,icons,stories"
     });
+    if (record.items.length === 0) {
+      alert("No data found for this date");
+      return;
+    }
+    if (!record.items[0].expand) {
+      alert("Data is Incomplete!")
+      return;
+    }
     record.items[0].date = new Date(record.items[0].date);
+    record.items[0].copticDate = record.items[0].expand.copticDate;
+    record.items[0].facts = record.items[0].expand.facts;
+    record.items[0].icons = record.items[0].expand.icons;
+    record.items[0].stories = record.items[0].expand.stories;
     setData(record.items[0]);
     // return record.items[0];
   }
@@ -31,7 +45,7 @@ function DatePill({ data, setData }: any) {
             <div className='flex items-center text-secondary font-bold justify-center bg-secondary-foreground rounded-full px-4 py-2'>
               <span className='text-sm flex items-center gap-2'>
                 {data.date.toLocaleDateString()} | {data.copticDate?.month}{" "}
-                {data.copticDate?.day} <ChevronDownIcon size={'20px'} />
+                {data.copticDate?.day} <ChevronDownIcon size={"20px"} />
               </span>
             </div>
           </div>
