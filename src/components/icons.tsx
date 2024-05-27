b5f42a99ddf933dcdc613229dee29270fb7d9b24
TechLion Dev (via GIT) <git@techlion.dev>
2024-05-27 14:04:31 -0400

    Slider ig, grouping broken still

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -8,23 +8,95 @@ import "swiper/css/effect-coverflow";
 function Icons({ data, setData }: any) {
   return (
     <>
-      <div className="flex w-full justify-center items-center">
-          <Swiper
-            effect={"coverflow"}
-            grabCursor={true}
-            centeredSlides={true}
-            slidesPerView={"auto"}
-            mousewheel={true}
-            modules={[EffectCoverflow, Pagination]}
-          >
-            {data.icons.map((icon: any, index: number) => (
-              <>
-                <SwiperSlide key={index} className="flex items-center justify-center">
-                  <img src={icon.image} className="h-60 rounded-lg self-center"/>
+      <div className='flex w-full justify-center items-center'>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            slideShadows: false,
+          }}
+          mousewheel={true}
+          modules={[EffectCoverflow]}
+        >
+          {data.icons.map((icon: any, index: number) => (
+            <>
+              <SwiperSlide
+                key={index}
+                className='flex items-center justify-center'
+              >
+                <img src={icon.image} className='h-60 rounded-lg self-center' />
+              </SwiperSlide>
+            </>
+          ))}
+          <SwiperSlide>
+              <Swiper
+                effect={"cards"}
+                grabCursor={true}
+                centeredSlides={false}
+                slidesPerView={"auto"}
+                mousewheel={true}
+                modules={[EffectCards]}
+                className='h-60 w-fit'
+              >
+                <SwiperSlide className='bg-primary h-60 rounded-lg'>
+                  <img
+                    src='https://agios-calendar.pockethost.io/api/files/nwill40feaquna2/7hcbmyqwty25hjc/the_honorable_archangeal_michael_BxLi9z70Rp.png'
+                    className='h-60 rounded-lg self-center'
+                  />
                 </SwiperSlide>
-              </>
-            ))}
-          </Swiper>
+                <SwiperSlide className='bg-primary h-60 rounded-lg'>
+                  <img
+                    src='https://agios-calendar.pockethost.io/api/files/nwill40feaquna2/7hcbmyqwty25hjc/the_honorable_archangeal_michael_BxLi9z70Rp.png'
+                    className='h-60 rounded-lg self-center'
+                  />
+                </SwiperSlide>
+                <SwiperSlide className='bg-primary h-60 rounded-lg'>
+                  <img
+                    src='https://agios-calendar.pockethost.io/api/files/nwill40feaquna2/7hcbmyqwty25hjc/the_honorable_archangeal_michael_BxLi9z70Rp.png'
+                    className='h-60 rounded-lg self-center'
+                  />{" "}
+                </SwiperSlide>
+                <SwiperSlide className='bg-primary h-60 rounded-lg'>
+                  <img
+                    src='https://agios-calendar.pockethost.io/api/files/nwill40feaquna2/7hcbmyqwty25hjc/the_honorable_archangeal_michael_BxLi9z70Rp.png'
+                    className='h-60 rounded-lg self-center'
+                  />{" "}
+                </SwiperSlide>
+                <SwiperSlide className='bg-primary h-60 rounded-lg'>
+                  <img
+                    src='https://agios-calendar.pockethost.io/api/files/nwill40feaquna2/7hcbmyqwty25hjc/the_honorable_archangeal_michael_BxLi9z70Rp.png'
+                    className='h-60 rounded-lg self-center'
+                  />{" "}
+                </SwiperSlide>
+                <SwiperSlide className='bg-primary h-60 rounded-lg'>
+                  <img
+                    src='https://agios-calendar.pockethost.io/api/files/nwill40feaquna2/7hcbmyqwty25hjc/the_honorable_archangeal_michael_BxLi9z70Rp.png'
+                    className='h-60 rounded-lg self-center'
+                  />{" "}
+                </SwiperSlide>
+                <SwiperSlide className='bg-primary h-60 rounded-lg'>
+                  <img
+                    src='https://agios-calendar.pockethost.io/api/files/nwill40feaquna2/7hcbmyqwty25hjc/the_honorable_archangeal_michael_BxLi9z70Rp.png'
+                    className='h-60 rounded-lg self-center'
+                  />{" "}
+                </SwiperSlide>
+                <SwiperSlide className='bg-primary h-60 rounded-lg'>
+                  <img
+                    src='https://agios-calendar.pockethost.io/api/files/nwill40feaquna2/7hcbmyqwty25hjc/the_honorable_archangeal_michael_BxLi9z70Rp.png'
+                    className='h-60 rounded-lg self-center'
+                  />{" "}
+                </SwiperSlide>
+                <SwiperSlide className='bg-primary h-60 rounded-lg'>
+                  <img
+                    src='https://agios-calendar.pockethost.io/api/files/nwill40feaquna2/7hcbmyqwty25hjc/the_honorable_archangeal_michael_BxLi9z70Rp.png'
+                    className='h-60 rounded-lg self-center'
+                  />{" "}
+                </SwiperSlide>
+              </Swiper>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </>
   );
